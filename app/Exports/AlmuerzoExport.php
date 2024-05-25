394707d75c1cde7d1f182a092d94bdd8a90acfd5
Jistoria <maestrojgcm@gmail.com>
2024-05-24 22:57:50 -0500

    prueba generar excel almuerzo

--- app/Exports/AlmuerzoExport.php
+++ app/Exports/AlmuerzoExport.php
@@ -3,10 +3,14 @@
 namespace App\Exports;
 
 use App\Models\Almuerzo;
+
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\FromQuery;
-class AlmuerzoExport implements FromQuery
+use Maatwebsite\Excel\Concerns\FromView;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+
+class AlmuerzoExport implements FromView, ShouldAutoSize
 {
     protected $date;
     public function __construct($date)
@@ -14,10 +18,12 @@ class AlmuerzoExport implements FromQuery
         $this->date = $date;
     }
 
-
-    public function query()
+    public function view(): \Illuminate\Contracts\View\View
     {
-        return Almuerzo::query()->where('horario_comida_id', $this->date);
+        return view('exports.almuerzo', [
+            'almuerzo' => Almuerzo::with(['deportisa','invitado','invitado.tipoInvitado','horario'])->where('horario_id', $this->date)->get()
+        ]);
     }
+
 }
 
---
+++ resources/views/exports/almuerzo.blade.php
@@ -0,0 +1,32 @@
+<table>
+    <thead>
+        <tr>
+            <th>Cedula</th>
+            <th>Nombre</th>
+            <th>Apellido</th>
+            <th>Tipo de Usuario</th>
+            <th>Fecha</th>
+            <th>Estado</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($almuerzos as $almuerzo)
+        <tr>
+            @if ($almuerzo->deportista)
+                <td>{{ $almuerzo->deportista->cedula }}</td>
+                <td>{{ $almuerzo->deportista->nombre }}</td>
+                <td>{{ $almuerzo->deportista->apellido }}</td>
+                <td>Atleta</td>
+            @endif
+            @if ($almuerzo->invitado)
+                <td>{{ $almuerzo->invitado->cedula }}</td>
+                <td>{{ $almuerzo->invitado->nombre }}</td>
+                <td>{{ $almuerzo->invitado->apellido }}</td>
+                <td>{{$almumerzo->invitado->tipo_invitado_nombre}}</td>
+            @endif
+            <td>{{ $almuerzo->horario->fecha }}</td>
+            <td>{{ $almuerzo->estado ? 'entregado' : 'no entregado'}}</td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
