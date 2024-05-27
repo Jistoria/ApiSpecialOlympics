eb4310ffc89fb4400bba238f96381713b36a2708
Jistoria <maestrojgcm@gmail.com>
2024-05-27 17:10:26 -0500

    correcion error null provincia

--- app/Models/Invitado.php
+++ app/Models/Invitado.php
@@ -53,7 +53,7 @@ class Invitado extends Model
             'qr' => Storage::exists($qrFilePath) ? Storage::get($qrFilePath) : null,
             'apellido' => $this->apellido,
             'url_imagen' => $this->url_imagen,
-            'provincia_id' => $this->provincia_id,
+            'provincia_id' => $this->provincia->provincia ?? null,
             'provincia' => $this->provincia->provincia,
             'tipo_invitado' => $this->tipoInvitado->tipo_invitado_nombre,
         ];
